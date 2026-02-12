51e5d3b975391400aae2e937dc7dbd6615e6cdf0
Tran Minh Khoa <tranminhkhoa2k42k5@gmail.com>
2026-02-13 00:42:34 +0700

    feat: add base folder structure for stream_processor module

--- src/ingestion/main.py
+++ src/ingestion/main.py
@@ -15,11 +15,13 @@ def main():
     logger.info("Ingestion service starting...")
 
     try:
+        # Load config
         logger.info("Loading configuration...")
         settings = load_settings()
         setup_logging()
         logger.info("Configuration loaded")
 
+        # Init Kafka Producer
         producer = KafkaProducerService(settings)
         data_root_path = Path(__file__).resolve().parent / "data"
         topics = {
@@ -28,14 +30,20 @@ def main():
             "person": settings.kafka.topics.person
         }
 
+        # Main logic
         logger.info("Start loading data...")
         record_flush_buffer = 0
         total_record_count = 0
         for record, data_type, data_label in iter_full(data_root_path):
+            # Enrich record
             enriched_record = enrich_record(record, data_type, data_label)
+
+            # Send record to kafka
             producer.send(topics[data_type], enriched_record)
             record_flush_buffer += 1
             total_record_count += 1
+
+            # Flush
             if record_flush_buffer >= settings.kafka.producer.max_buffer:
                 producer.flush()
                 record_flush_buffer = 0
